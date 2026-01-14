fcd1b99e7eb5cdfa550140cc89368ac93b0090d4
saraprabs <vsaranya04@gmail.com>
2026-01-14 08:53:06 +0100

    Commit

--- LAB 1/4a-b.py
+++ LAB 1/4a-b.py
@@ -5,4 +5,6 @@ speed = input("Enter the average speed per hour: ")
 distance = float(distance)
 speed = float(speed)
 time_taken = distance / speed
-print(f"I takes {time_taken:.f} hours to travel from Stockholm to Gothenburg ")
+print(f"I takes {time_taken:.2f} hours to travel from Stockholm to Gothenburg ")
+print(f"I takes {time_taken * 60:.2f} hours to travel from Stockholm to Gothenburg ")
+
