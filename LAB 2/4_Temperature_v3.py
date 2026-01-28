18eafdbbfc0afb5cca1920cc6c66e1653771d4a5
saraprabs <vsaranya04@gmail.com>
2026-01-28 15:06:15 +0100

    lab2  changes commit

--- LAB 2/4_Temperature_v3.py
+++ LAB 2/4_Temperature_v3.py
@@ -6,23 +6,21 @@ Check if temperature below 10 Or above 20 and provide clothing suggestions
 print("\n************* TEMPERATURE CONVERSION *************")
 print("\nDo you want to convert from Celsius to Fahrenheit / Fahrenheit to Celsius?")
 option_temperature = input(" Enter ( C or F): ")
+if option_temperature == "C" or option_temperature == "c"  or option_temperature == "F" or option_temperature == "f":
+    if option_temperature == "C" or option_temperature == "c":
+        celsius = float(input("\nEnter a temperature in degrees Celsius: "))
+        fahrenheit = (celsius * 1.8) + 32
+        print(f"\n Temperature is {fahrenheit:.2f} °F  Fahrenheit")
+    else:
+        fahrenheit = float(input("\nEnter a temperature in degrees Fahrenheit: "))
+        celsius = (fahrenheit - 32) * 5/9
+        print(f"\n Temperature is {celsius:.2f} °C Celsius")
 
-if option_temperature == "C" or option_temperature == "c":
-    celsius = float(input("\nEnter a temperature in degrees Celsius: "))
-    fahrenheit = (celsius * 1.8) + 32
-    print(f"\n Temperature is {fahrenheit:.2f} °F  Fahrenheit")
-    if int(celsius) < 10:
+    if celsius < 10:
         print("\n Temperature is below 10 °C. Gear Up for Winter!!")
-    elif int(celsius) > 20:
+    elif celsius > 20:
         print("\n Temperature is above 20 °C. Enjoy Sunny day!! Pack your swim wear.")
-elif option_temperature == "F" or option_temperature == "f":
-    fahrenheit = float(input("\nEnter a temperature in degrees Fahrenheit: "))
-    celsius = (fahrenheit - 32) * 5/9
-    print(f"\n Temperature is {celsius:.2f} °C Celsius")
-    if int(celsius) < 10:
-        print("\n Temperature is below 10 °C. Gear Up for Winter!!")
-    elif int(celsius) > 20:
-        print("\n Temperature is above 20 °C. Enjoy Sunny day!! Pack your swim wear.")
+
 else:
     print("\nInvalid input. Please enter a valid input.")
 
