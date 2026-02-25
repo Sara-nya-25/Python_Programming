0633483cc46cb0357b879f511ac3a5643a600461
saraprabs <vsaranya04@gmail.com>
2026-02-25 13:36:17 +0100

    update move to check wall, pickup & add points

--- Game/Fruit_loop/player.py
+++ Game/Fruit_loop/player.py
@@ -1,15 +1,32 @@
+from grid import Grid
+
 class Player:
     marker = "@"
 
     def __init__(self, x, y):
         self.pos_x = x
         self.pos_y = y
+        self.score = 0
 
-    def move(self, dx, dy):
-        self.pos_x += dx
-        self.pos_y += dy
+    def move(self, dx, dy, grid):
+        # Adjust move only if path is clear
+        if self.can_move(dx, dy, grid):
+            self.pos_x += dx
+            self.pos_y += dy
 
-    def can_move(self, x, y, grid):
+        current_tile = grid.get(self.pos_x, self.pos_y)
+        if current_tile == "S":  # Fruit Salad
+            self.score += 20
+            grid.clear(self.pos_x, self.pos_y)
+        elif current_tile == "o":  # Standard Fruit
+            self.score += 10
+            grid.clear(self.pos_x, self.pos_y)
+        else:
+            # Optional: feedback for hitting a wall
+            pass
+
+
+    def can_move(self, dx, dy, grid):
         """Checks if the intended next tile is a wall or out of bounds."""
         new_x = self.pos_x + dx
         new_y = self.pos_y + dy
